6b84d94a5901ece389fe4de059272569944a0091
s31384 <s31384@pjwstk.edu.pl>
2026-03-31 15:08:27 +0200

    nai

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Iris> trainingSet = new ArrayList<>();
+        List<Iris> testSet = new ArrayList<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader("iris.txt"))){
             String line = br.readLine();
             int i = 0;
@@ -19,11 +21,45 @@ public class Main {
                 double petalWidth = Double.parseDouble(tokens[3]);
                 String type = tokens[4];
                 Iris iris = new Iris(sepalLength, sepalWidth, petalLength, petalWidth, type);
-
+                if(i % 5 == 0){
+                    trainingSet.add(iris);
+                }else testSet.add(iris);
+                i++;
             }
         }catch (Exception e){
             e.printStackTrace();
         }
+        Collections.shuffle(trainingSet);
+        Collections.shuffle(testSet);
 
+
+        Perceptron perceptron = new Perceptron(new double[]{0,0,0,0} , 0.5, 0.01, 0.01 );
+        for(int i = 0; i < 5; i++){
+        for(Iris iris: trainingSet){
+            if(iris.getType() == "Iris-setosa"){
+                perceptron.learn(iris.getVector(), 1);
+            }else {perceptron.learn(iris.getVector(), 0);}
+        }
+
+        }
+
+        int rightAnswers = 0;
+
+        for(Iris iris: testSet){
+            int setosa = 0;
+            if(iris.getType() == "Iris-setosa"){
+                setosa =1 ;
+            }
+
+            int classifed = perceptron.classify(iris.getVector());
+            if(setosa == classifed){
+                rightAnswers++;
+            }
+
+            System.out.println(iris.getType() + " " + classifed );
+        }
+        double accuracy = rightAnswers/testSet.size() * 100;
+
+        System.out.println("Accuracy: " + accuracy);
     }
 }
